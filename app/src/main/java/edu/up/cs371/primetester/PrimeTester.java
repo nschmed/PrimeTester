756a063f331f8407d7e4329000f5f8b2b58a15ff
schmedak15 <schmedak15@up.edu>
2015-09-02 16:37:05 -0700

    Working isPrime method and correct indentation

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,7 +14,15 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+        //check for 0,1
+        if(n==0 || n==1){
+            return false;
+        }
+        for(int i=2; i<n;i++){
+            if (n%i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
